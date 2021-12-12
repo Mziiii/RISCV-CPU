f3bb541108828972ac50fbf3fec122fd222ed6c0
Mzi <74357423+Mziiii@users.noreply.github.com>
2021-12-12 23:45:16 +0800

    ...

---
+++ riscv/src/alu.v
@@ -0,0 +1,27 @@
+// `include "config.v"
+
+// module alu (
+//     input wire clk,
+//     input wire rst,
+//     input wire rdy,
+
+//     // request(add-inst) from alu_rs
+//     input wire alu_en_i,
+//     input wire [`OpBus] opcode_i,
+//     input wire [`DataBus] rs1_i,
+//     input wire [`DataBus] rs2_i,
+//     input wire [`] ;
+
+
+//     input wire [`DataBus] 
+// );
+
+// always @(*) begin
+    
+// end
+
+// always @(posedge clk) begin
+    
+// end
+    
+// endmodule
