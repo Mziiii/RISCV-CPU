16499cda6ea04801179247ca2bd30b81e6e43a7f
your user name <your user email>
2022-02-02 00:19:06 +0800

    fix SLB

--- riscv/.history/src/slb_20220202000128.v
+++ riscv/.history/src/slb_20220202000128.v
@@ -84,16 +84,16 @@
     always @(posedge clk) begin
         if (rst||clr) begin
             for (i = 0; i< `SLBNum; i = i+1) begin
-                ls[i]        <= 1'b0;
-                rs1_dt[i]    <= 0;
-                rs2_dt[i]    <= 0;
-                rs1_nick[i]  <= 0;
-                rs2_nick[i]  <= 0;
-                imm[i]       <= 0;
-                op[i]        <= 0;
-                pc[i]        <= 0;
-                rs1_valid[i] <= 1'b0;
-                rs2_valid[i] <= 1'b0;
+                ls[i]       <= 1'b0;
+                rs1_dt[i]   <= 0;
+                rs2_dt[i]   <= 0;
+                rs1_nick[i] <= 0;
+                rs2_nick[i] <= 0;
+                imm[i]      <= 0;
+                op[i]       <= 0;
+                pc[i]       <= 0;
+                rs1_valid[i]<= 1'b0;
+                rs2_valid[i]<= 1'b0;
             end
             occupied <= 0;
         end
@@ -143,54 +143,54 @@
                 oSLB_dt             <= iDC_dt;
                 oROB_dt             <= iDC_dt;
                 occupied[iDC_nick]  <= 1'b0;
-                rs1_valid[iDC_nick] <= 1'b0;
-                rs1_nick[iDC_nick]  <= 0;
-                rs1_dt[iDC_nick]    <= 0;
-                rs2_valid[iDC_nick] <= 1'b0;
-                rs2_nick[iDC_nick]  <= 0;
-                rs2_dt[iDC_nick]    <= 0;
-                imm[iDC_nick]       <= 0;
-                op[iDC_nick]        <= 0;
+                rs1_valid[iDC_nick] <=1'b0;
+                rs1_nick[iDC_nick]  <=0;
+                rs1_dt[iDC_nick]    <=0;
+                rs2_valid[iDC_nick] <=1'b0;
+                rs2_nick[iDC_nick]  <=0;
+                rs2_dt[iDC_nick]    <=0;
+                imm[iDC_nick]       <=0;
+                op[iDC_nick]        <=0;
                 pc[iDC_nick]        <= 0;
-                ls[iDC_nick]        <= 0;
+                ls[iDC_nick]        <=0;
             end
 
-            oDC_en <= 1'b0;
+            oDC_en<=1'b0;
             if(valid) begin
                 if(iROB_commit_en) begin
                     if(occupied[iROB_commit_nick]&&rs1_valid[iROB_commit_nick]&&rs2_valid[iROB_commit_nick])begin
-                        oDC_en   <= 1'b1;
-                        oDC_ls   <= `Store;
-                        oDC_addr <= rs1_dt[iROB_commit_nick] + imm[iROB_commit_nick];
-                        oDC_dt   <= rs2_dt[iROB_commit_nick];
-                        oDC_nick <= iROB_commit_nick;
+                        oDC_en<=1'b1;
+                        oDC_ls<=`Store;
+                        oDC_addr<=rs1_dt[iROB_commit_nick]+imm[iROB_commit_nick];
+                        oDC_dt<=rs2_dt[iROB_commit_nick];
+                        oDC_nick<=iROB_commit_nick;
                         case (op[iROB_commit_nick])
                             `SB: oDC_len <= `One;
                             `SH:oDC_len <= `Two;
                             `SW:oDC_len <=`Four;
                             default; 
                         endcase
-                        rs1_valid[iROB_commit_nick] <= 1'b0;
-                        rs1_nick[iROB_commit_nick]  <= 0;
-                        rs1_dt[iROB_commit_nick]    <= 0;
+                        rs1_valid[iROB_commit_nick]<=1'b0;
+                        rs1_nick[iROB_commit_nick]<=0;
+                        rs1_dt[iROB_commit_nick]<=0;
                         rs2_valid[iROB_commit_nick] <= 1'b0;
-                        rs2_nick[iROB_commit_nick]  <= 0;
-                        rs2_dt[iROB_commit_nick]    <= 0;
-                        imm[iROB_commit_nick]       <= 0;
-                        op[iROB_commit_nick]        <= 0;
-                        pc[iROB_commit_nick]        <= 0;
-                        ls[iROB_commit_nick]        <= 0;
-                        occupied[iROB_commit_nick]  <= 1'b0;
+                        rs2_nick[iROB_commit_nick]<=0;
+                        rs2_dt[iROB_commit_nick]<=0;
+                        imm[iROB_commit_nick]<=0;
+                        op[iROB_commit_nick]<=0;
+                        pc[iROB_commit_nick]<=0;
+                        ls[iROB_commit_nick]<=0;
+                        occupied[iROB_commit_nick]<=1'b0;
                     end
                 end
                 else begin
-                    for (i = `RSNum - 1; i >= 0; i = i - 1 ) begin
+                    for (i = `RSNum-1; i>=0; i=i-1 ) begin
                         if(occupied[i]&&rs1_valid[i]&&ls[i]==`Load) begin
-                            oDC_en   <= 1'b1;
-                            oDC_ls   <= `Load;
-                            oDC_nick <= i[`NickBus];
-                            oDC_dt   <= 0;
-                            oDC_addr <= rs1_dt[i] + imm[i];
+                            oDC_en<=1'b1;
+                            oDC_ls<=`Load;
+                            oDC_nick<=i[`NickBus];
+                            oDC_dt<=0;
+                            oDC_addr<=rs1_dt[i]+imm[i];
                             case (op[i])
                                 `LB,
                                 `LBU:oDC_len <= `One;
@@ -205,33 +205,21 @@
             end
 
             if (iDP_en) begin
-                case(iDP_op)
-                `SB,
-                `SH,
-                `SW,
-                `LB,
-                `LBU,
-                `LH,
-                `LHU,
-                `LW:begin
-                    occupied[idx]  <= 1'b1;
-                    op[idx]        <= iDP_op;
-                    pc[idx]        <= iDP_pc;
-                    imm[idx]       <= iDP_imm;
-                    rs1_nick[idx]  <= iDP_rs1_nick;
-                    rs2_nick[idx]  <= iDP_rs2_nick;
-                    rs1_dt[idx]    <= iDP_rs1_dt;
-                    rs2_dt[idx]    <= iDP_rs2_dt;
-                    rs1_valid[idx] <= iDP_rs1_nick == 0?1'b1:1'b0;
-                    rs2_valid[idx] <= iDP_rs2_nick == 0?1'b1:1'b0;
-                    case (iDP_op)
+                occupied[idx] <= 1'b1;
+                op[idx]       <= iDP_op;
+                pc[idx]       <= iDP_pc;
+                imm[idx]      <= iDP_imm;
+                rs1_nick[idx] <= iDP_rs1_nick;
+                rs2_nick[idx] <= iDP_rs2_nick;
+                rs1_dt[idx]   <= iDP_rs1_dt;
+                rs2_dt[idx]   <= iDP_rs2_dt;
+                rs1_valid[idx]<= iDP_rs1_nick == 0?1'b0:1'b1;
+                rs2_valid[idx]<= iDP_rs2_nick == 0?1'b0:1'b1;
+                case (iDP_op)
                     `SB,
                     `SH,
                     `SW:ls[idx]      <= `Store;
                     default: ls[idx] <= `Load;
-                    endcase
-                end
-                default;
                 endcase
             end
         end
