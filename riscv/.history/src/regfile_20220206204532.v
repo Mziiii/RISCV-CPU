c5dba4be6d8418d9d3195db9d0c48cea084332a6
your user name <your user email>
2022-02-06 22:31:11 +0800

    fix sth. with zero reg

---
+++ riscv/.history/src/regfile_20220206204532.v
@@ -0,0 +1,223 @@
+`include "config.v"
+
+module regfile(
+    input wire clk,
+    input wire rst,
+    input wire rdy,
+
+    input wire clr,
+
+    input wire            iIND_en,
+    input wire [`NameBus] iIND_rs1_regnm,
+    input wire [`NameBus] iIND_rs2_regnm,
+    output reg [`NickBus] oDP_rs1_nick,
+    output reg [`NickBus] oDP_rs2_nick,
+    output reg [`DataBus] oDP_rs1_dt,
+    output reg [`DataBus] oDP_rs2_dt,
+    //when iIND_en they can be taken to dispatch as well
+    input wire [`NameBus] iIND_rd_regnm,
+    input wire [`OpBus]   iIND_op,
+    input wire [`AddrBus] iIND_pc,
+    input wire [`ImmBus]  iIND_imm,
+    input wire            iIND_pd,
+    output reg            oDP_en,
+    output reg [`NameBus] oDP_rd_regnm,
+    output reg [`OpBus]   oDP_op,
+    output reg [`AddrBus] oDP_pc,
+    output reg [`ImmBus]  oDP_imm,
+    output reg            oDP_pd,
+
+    //in the progress of dispatch
+    input wire            iROB_nick_en,
+    input wire [`NameBus] iROB_nick_regnm,
+    input wire [`NickBus] iROB_nick,   
+
+    //in the progress of commit
+    input wire            iROB_en,
+    input wire [`NameBus] iROB_rd_regnm,
+    input wire [`DataBus] iROB_rd_dt,
+    input wire [`NickBus] iROB_rd_nick
+);
+    
+    reg [`DataBus] reg_dt[`RegNumBus];
+    reg [`NickBus] reg_nick[`RegNumBus];
+
+//todo:debug!
+    wire [`DataBus] ra_reg_data=reg_dt[1];
+    wire [`DataBus] sp_reg_data=reg_dt[2];
+    wire [`DataBus] gp_reg_data=reg_dt[3];
+    wire [`DataBus] tp_reg_data=reg_dt[4];
+    wire [`DataBus] t0_reg_data=reg_dt[5];
+    wire [`DataBus] t1_reg_data=reg_dt[6];
+    wire [`DataBus] t2_reg_data=reg_dt[7];
+    wire [`DataBus] s0_reg_data=reg_dt[8];
+    wire [`DataBus] s1_reg_data=reg_dt[9];
+    wire [`DataBus] a0_reg_data=reg_dt[10];
+    wire [`DataBus] a1_reg_data=reg_dt[11];
+    wire [`DataBus] a2_reg_data=reg_dt[12];
+    wire [`DataBus] a3_reg_data=reg_dt[13];
+    wire [`DataBus] a4_reg_data=reg_dt[14];
+    wire [`DataBus] a5_reg_data=reg_dt[15];
+    wire [`DataBus] a6_reg_data=reg_dt[16];
+    wire [`DataBus] a7_reg_data=reg_dt[17];
+    wire [`DataBus] s2_reg_data=reg_dt[18];
+    wire [`DataBus] s3_reg_data=reg_dt[19];
+    wire [`DataBus] s4_reg_data=reg_dt[20];
+    wire [`DataBus] s5_reg_data=reg_dt[21];
+    wire [`DataBus] s6_reg_data=reg_dt[22];
+    wire [`DataBus] s7_reg_data=reg_dt[23];
+    wire [`DataBus] s8_reg_data=reg_dt[24];
+    wire [`DataBus] s9_reg_data=reg_dt[25];
+    wire [`DataBus] s10_reg_data=reg_dt[26];
+    wire [`DataBus] s11_reg_data=reg_dt[27];
+    wire [`DataBus] t3_reg_data=reg_dt[28];
+    wire [`DataBus] t4_reg_data=reg_dt[29];
+    wire [`DataBus] t5_reg_data=reg_dt[30];
+    wire [`DataBus] t6_reg_data=reg_dt[31];
+
+    wire [`NickBus] ra_reg_nick=reg_nick[1];    
+    wire [`NickBus] sp_reg_nick=reg_nick[2];
+    wire [`NickBus] gp_reg_nick=reg_nick[3];
+    wire [`NickBus] tp_reg_nick=reg_nick[4];
+    wire [`NickBus] t0_reg_nick=reg_nick[5];
+    wire [`NickBus] t1_reg_nick=reg_nick[6];
+    wire [`NickBus] t2_reg_nick=reg_nick[7];
+    wire [`NickBus] s0_reg_nick=reg_nick[8];
+    wire [`NickBus] s1_reg_nick=reg_nick[9];
+    wire [`NickBus] a0_reg_nick=reg_nick[10];
+    wire [`NickBus] a1_reg_nick=reg_nick[11];
+    wire [`NickBus] a2_reg_nick=reg_nick[12];
+    wire [`NickBus] a3_reg_nick=reg_nick[13];
+    wire [`NickBus] a4_reg_nick=reg_nick[14];
+    wire [`NickBus] a5_reg_nick=reg_nick[15];
+    wire [`NickBus] a6_reg_nick=reg_nick[16];
+    wire [`NickBus] a7_reg_nick=reg_nick[17];
+    wire [`NickBus] s2_reg_nick=reg_nick[18];
+    wire [`NickBus] s3_reg_nick=reg_nick[19];
+    wire [`NickBus] s4_reg_nick=reg_nick[20];
+    wire [`NickBus] s5_reg_nick=reg_nick[21];
+    wire [`NickBus] s6_reg_nick=reg_nick[22];
+    wire [`NickBus] s7_reg_nick=reg_nick[23];
+    wire [`NickBus] s8_reg_nick=reg_nick[24];
+    wire [`NickBus] s9_reg_nick=reg_nick[25];
+    wire [`NickBus] s10_reg_nick=reg_nick[26];
+    wire [`NickBus] s11_reg_nick=reg_nick[27];
+    wire [`NickBus] t3_reg_nick=reg_nick[28];
+    wire [`NickBus] t4_reg_nick=reg_nick[29];
+    wire [`NickBus] t5_reg_nick=reg_nick[30];
+    wire [`NickBus] t6_reg_nick=reg_nick[31];
+
+    integer i;
+
+    always @(*) begin
+        if (rst) begin
+            oDP_en       = 1'b0;
+            oDP_rs1_dt   = 0;
+            oDP_rs2_dt   = 0;
+            oDP_rs1_nick = 0;
+            oDP_rs2_nick = 0;
+            oDP_imm      = 0;
+            oDP_op       = 0;
+            oDP_pc       = 0;
+            oDP_pd       = 0;
+            oDP_rd_regnm = 0;
+        end
+        else if (clr) begin
+            oDP_en       = 1'b0;
+            oDP_rs1_dt   = 0;
+            oDP_rs2_dt   = 0;
+            oDP_rs1_nick = 0;
+            oDP_rs2_nick = 0;
+            oDP_imm      = 0;
+            oDP_op       = 0;
+            oDP_pc       = 0;
+            oDP_pd       = 0;
+            oDP_rd_regnm = 0;
+        end
+        else if (rdy) begin
+            if(iIND_en) begin
+                oDP_en       = 1'b1;
+                oDP_rs1_dt   = reg_dt[iIND_rs1_regnm];
+                oDP_rs2_dt   = reg_dt[iIND_rs2_regnm];
+                oDP_rs1_nick = reg_nick[iIND_rs1_regnm];
+                oDP_rs2_nick = reg_nick[iIND_rs2_regnm];
+
+                //others taken as well
+                oDP_imm      = iIND_imm;
+                oDP_op       = iIND_op;
+                oDP_pc       = iIND_pc;
+                oDP_pd       = iIND_pd;
+                oDP_rd_regnm = iIND_rd_regnm;
+            end else begin
+                oDP_en       = 1'b0;
+                oDP_rs1_dt   = 0;
+                oDP_rs2_dt   = 0;
+                oDP_rs1_nick = 0;
+                oDP_rs2_nick = 0;
+                oDP_imm      = 0;
+                oDP_op       = 0;
+                oDP_pc       = 0;
+                oDP_pd       = 0;
+                oDP_rd_regnm = 0;
+            end
+        end else begin
+            oDP_en       = 1'b0;
+            oDP_rs1_dt   = 0;
+            oDP_rs2_dt   = 0;
+            oDP_rs1_nick = 0;
+            oDP_rs2_nick = 0;
+            oDP_imm      = 0;
+            oDP_op       = 0;
+            oDP_pc       = 0;
+            oDP_pd       = 0;
+            oDP_rd_regnm = 0;
+        end
+    end
+
+    always @(posedge clk) begin
+        if(rst) begin
+            for (i = 0 ;i < `RegNum ;i = i + 1) begin
+                reg_dt[i]   <= 0;
+                reg_nick[i] <= 0;
+            end
+        end else if (clr) begin
+            reg_dt[iROB_rd_regnm] = iROB_rd_dt;
+            for (i = 0 ;i < `RegNum ;i = i + 1) begin
+                reg_nick[i] = 0;
+            end
+        end else if (rdy) begin
+            if (iROB_en) begin
+                if (iROB_rd_regnm != 0reg_nick[iROB_rd_regnm] == iROB_rd_nick) begin
+                    reg_nick[iROB_rd_regnm] = 0;
+                    reg_dt[iROB_rd_regnm]   = iROB_rd_dt;
+                end
+            end
+            
+            if (iROB_nick_en) begin
+                //todo:sb,sh,sw:reg_rd->not write in nick
+                case(iIND_op) 
+                `SB,
+                `SH,
+                `SW,
+                `BEQ,
+                `BNE,
+                `BLT,
+                `BGE,
+                `BLTU,
+                `BGEU:begin
+                    
+                end
+                default:begin
+                    if (iROB_nick_regnm != 0)
+                    reg_nick[iROB_nick_regnm] = iROB_nick;
+                end
+                endcase
+            end
+        end else begin
+            for (i = 0 ;i < `RegNum ;i = i + 1) begin
+                reg_dt[i]   <= 0;
+                reg_nick[i] <= 0;
+            end
+        end
+    end
+endmodule
---
+++ riscv/.history/src/rob_20220206122411.v
@@ -0,0 +1,258 @@
+`include "config.v"
+
+module rob (
+    input wire clk,
+    input wire rst,
+    input wire rdy,
+
+    output reg clr,
+    output reg [`AddrBus] oINF_j_pc,
+
+    input wire iclr,
+    //inf 
+    output reg            oINF_full,
+
+    input wire            iIND_en,
+    input wire [`NameBus] iIND_rd_regnm,
+    //send nick to dispatch(->rf/dispatch)
+    output reg            oROB_nick_en,
+    output reg [`NickBus] oROB_nick,
+    output reg [`NameBus] oROB_nick_regnm,
+
+    //dispatch to rs/slb
+    input wire            iDP_en,
+    input wire [`OpBus]   iDP_op,
+    input wire            iDP_pd,
+    input wire [`NickBus] iDP_rd_nick,
+    input wire [`NickBus] iDP_rd_regnm,
+
+    input wire [`AddrBus] iDP_pc,//todo:debug!!!!!!!!!!
+
+
+    //ex
+    input wire            iEX_en,
+    input wire [`NickBus] iEX_nick,
+    input wire [`DataBus] iEX_dt,
+    input wire            iEX_ac,
+    input wire [`AddrBus] iEX_j_pc,
+
+    //slb
+    input wire            iSLB_en,
+    input wire [`NickBus] iSLB_nick,
+    input wire [`DataBus] iSLB_dt,
+
+    //
+    output reg            oSLB_store_en,
+    output reg [`NickBus] oSLB_store_nick,
+
+    //commit: write back to regfile 
+    output reg            oRF_en,
+    output reg [`NameBus] oRF_rd_regnm,
+    output reg [`DataBus] oRF_rd_dt,
+    output reg [`NickBus] oRF_rd_nick
+);
+    
+    //fifo
+    reg [`RobBus]  occupied;
+    reg            commit[`RobBus];
+    reg [`NameBus] regnm[`RobBus];
+    reg [`DataBus] dt[`RobBus];
+    reg            ls[`RobBus];//load_store: 1'b1(`Store) only when being store.1'b0 represents the other cases
+    reg            ac[`RobBus];//actually
+    reg            pd[`RobBus];//preditcted
+    reg [`AddrBus] j_pc[`RobBus];//jumped_pc
+    
+    reg [`NickBus] rd_ptr,wt_ptr;
+    wire full  = &occupied;
+    wire empty = !(|occupied);
+    
+    wire [`NickBus] rd_nx_ptr;
+    assign rd_nx_ptr = (rd_ptr == 5'b11111)?1:rd_ptr+1;
+
+    integer i;
+
+    //todo:debug!!!!!!!!!!!!!!
+    reg [`AddrBus] pc[`RobBus];
+    wire debug_commit=commit[1];
+    wire [`NameBus] debug_regnm=regnm[1];
+    wire [`DataBus] debug_dt=dt[1];
+    wire debug_ls=ls[1];
+    wire debug_ac=ac[1];
+    wire debug_pd=pd[1];
+    wire [`AddrBus] debug_j_pc=j_pc[1];
+
+    //dispatch and commit
+    always @(*) begin
+        if(rst||iclr) begin
+            oINF_full       = 1'b0;
+            oROB_nick_en    = 1'b0;
+            oROB_nick       = 0;
+            oROB_nick_regnm = 0;
+            wt_ptr          = 1;
+        end 
+        else if(rdy) begin
+            oINF_full = full;
+            if(iIND_en&&!full) begin
+                oROB_nick_en    = 1'b1;
+                oROB_nick       = wt_ptr;
+                oROB_nick_regnm = iIND_rd_regnm;
+                if(wt_ptr!=5'b11111) wt_ptr = wt_ptr + 1;
+                else wt_ptr=1;
+            end 
+            else begin
+                oROB_nick_en    = 1'b0;
+                oROB_nick       = 0;
+                oROB_nick_regnm = 0;
+            end
+        end else begin
+            oINF_full       = 0;
+            oROB_nick_en    = 1'b0;
+            oROB_nick       = 0;
+            oROB_nick_regnm = 0;
+        end
+    end
+
+    always @(*) begin
+        if(rst||iclr) begin
+            //output
+            oRF_en         = 1'b0;
+            oSLB_store_en  = 1'b0;
+        end else if(rdy) begin
+            if (!empty && (commit[rd_ptr] || ls[rd_ptr])) begin
+                if (ls[rd_ptr]) begin
+                    oRF_en           = 1'b0;
+                    oSLB_store_en    = 1'b1;
+                    oSLB_store_nick  = rd_ptr;
+                end
+                else if (pd[rd_ptr] != ac[rd_ptr]) begin
+                    oSLB_store_en    = 1'b0;
+                    oINF_j_pc        = j_pc[rd_ptr];
+                    oRF_en           = 1'b1;
+                    oRF_rd_dt        = dt[rd_ptr];
+                    oRF_rd_nick      = rd_ptr;
+                    oRF_rd_regnm     = regnm[rd_ptr];
+                end
+                else begin
+                    //read from fifo when !empty
+                    oSLB_store_en    = 1'b0;
+                    oRF_en           = 1'b1;
+                    oRF_rd_dt        = dt[rd_ptr];
+                    oRF_rd_nick      = rd_ptr;
+                    oRF_rd_regnm     = regnm[rd_ptr];
+                end
+            end else begin
+                oRF_en         = 1'b0;
+                oSLB_store_en  = 1'b0;
+            end
+        end else begin
+            //output
+            oRF_en         = 1'b0;
+            oSLB_store_en  = 1'b0;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (rst||iclr) begin
+            for(i = 0; i < `RobNum; i = i + 1) begin
+                regnm[i] <= 0;
+                dt[i]    <= 0;
+                commit[i]<= 1'b0;
+                j_pc[i]  <= 0;
+                ac[i]    <= `NotJump;
+                pd[i]    <= `NotJump;
+                ls[i]    <= 1'b0;
+            end
+            occupied       <= 0;
+            rd_ptr         <= 1;
+            clr            <= 1'b0;
+        end
+        else if (rdy) begin
+
+            if(iDP_en) begin
+                regnm[iDP_rd_nick]    <= iDP_rd_regnm;
+                occupied[iDP_rd_nick] <= 1'b1;
+                pd[iDP_rd_nick]       <= iDP_pd;
+                case(iDP_op) 
+                `SB,
+                `SH,
+                `SW:
+                ls[iDP_rd_nick] <= `Store;
+                default;
+                endcase
+
+               //todo:debug!!!!!!!!!!!!!
+                pc[iDP_rd_nick] <= iDP_pc;
+
+            end
+            
+            if (!empty && commit[rd_ptr]) begin
+                if (pd[rd_ptr] != ac[rd_ptr]) begin
+                    clr              <= 1'b1;
+                end
+                else begin
+                    //clr when commit
+                    clr              <= 1'b0;
+                    occupied[rd_ptr] <= 1'b0;
+                    regnm[rd_ptr]    <= 0;
+                    dt[rd_ptr]       <= 0;
+                    commit[rd_ptr]   <= 1'b0;
+                    j_pc[rd_ptr]     <= 0;
+                    ac[rd_ptr]       <= `NotJump;
+                    pd[rd_ptr]       <= `NotJump;
+                    ls[rd_ptr]       <= 1'b0;
+
+                    //todo:debug!!!!!!!!!!!!!
+                    pc[rd_ptr] <= 0;
+
+                    //
+                    rd_ptr <= rd_nx_ptr;
+                end
+            end else begin
+                clr <= 1'b0;
+            end
+
+            if (iEX_en) begin
+                dt[iEX_nick]     <= iEX_dt;
+                ac[iEX_nick]     <= iEX_ac;
+                commit[iEX_nick] <= 1'b1;
+                j_pc[iEX_nick]   <= iEX_j_pc;
+            end
+            if (iSLB_en) begin
+                if(ls[iSLB_nick]) begin
+                    occupied[rd_ptr] <= 1'b0;
+                    regnm[rd_ptr]    <= 0;
+                    dt[rd_ptr]       <= 0;
+                    commit[rd_ptr]   <= 1'b0;
+                    j_pc[rd_ptr]     <= 0;
+                    ac[rd_ptr]       <= `NotJump;
+                    pd[rd_ptr]       <= `NotJump;
+                    ls[rd_ptr]       <= 1'b0;
+                end
+                dt[iSLB_nick]      <= iSLB_dt;
+                commit[iSLB_nick]  <= 1'b1;
+            end
+
+            //todo:debug!!!!!!!!!!
+            if (!empty && commit[rd_ptr]) begin
+                $display("%h",pc[rd_ptr][15:0]);
+                // if (oRF_rd_regnm!=0) begin
+                    $display("reg[%0h] %0h",oRF_rd_regnm,oRF_rd_dt);
+                // end
+            end
+            // if (control_hazard) begin
+            //     $display("control hazard at %h",pre_pc_queue[q_rd_ptr][15:0]);
+            // end
+            // $display("%b %b %b %b %b %h",control_hazard,d_empty,q_empty,wr_en_prot,isStore_input,has_value);
+            // if (rd_en_prot) begin
+            //     $display("%h %h %h",pre_pc_queue[q_rd_ptr],rob_predict_pc[q_rd_ptr],rob_npc[q_rd_ptr]);
+            //     $display("%h %h %h",commit_modify_regfile,commit_reg_addr,Commit_V);
+            //     if (commit_modify_regfile) begin
+            //         $display("%h %h",commit_reg_addr,Commit_V);
+            //     end
+            //     //$display("%d",pre_pc_queue[q_rd_ptr]);
+            // end
+        end else begin
+            clr <= 1'b0;
+        end
+    end
+endmodule
